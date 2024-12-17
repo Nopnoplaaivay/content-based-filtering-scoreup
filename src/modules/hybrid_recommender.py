332f140c97670441681b56b538cefc1c0ff1e696
KhangVinh <vinhkhang378@gmail.com>
2024-12-17 09:43:26 +0700

    update

---
+++ src/modules/hybrid_recommender.py
@@ -0,0 +1,18 @@
+import random
+
+from src.db import Ratings, Logs, Users
+from src.modules.content_based_recommender import CBFRecommender
+from src.modules.spaced_repetition_recommender import LSRRecommender
+from src.modules.learner_level_recommender import LLRRecommender
+from src.utils.logger import LOGGER
+
+
+class HybridRecommender:
+
+    def __init__(self):
+        self.cb_recommender = CBFRecommender()
+        self.lsr_recommender = LSRRecommender()
+
+    def recommendation_list(self, user_id, max_exercises=5):
+        cbf_list = self.cb_recommender.priority_list(user_id)
+        print(cbf_list)
